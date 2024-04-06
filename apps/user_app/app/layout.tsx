d0c34917ddca38afbd2368686495a0dd6eeb349b
Prathyarthi <prathyarti@gmail.com>
2024-04-06 23:53:57 +0530

    Modified layout and root page

--- apps/user_app/app/layout.tsx
+++ apps/user_app/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../providers";
+import { AppbarClient } from "../components/AppbarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <Providers>
         <body className={inter.className}>
+          <AppbarClient />
           {children}
         </body>
       </Providers>
